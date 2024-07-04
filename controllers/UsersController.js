68422b86c28a5abdd43f7b132ebffe6188015e8c
ZorEl212 <yabsirad212@proton.me>
2024-07-03 21:21:12 -0400

    Controller: Implement user controller to handle user related requests
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

---
+++ controllers/UsersController.js
@@ -0,0 +1,37 @@
+// controllers/UsersController.js
+const crypto = require('crypto');
+const dbClient = require('../utils/db');
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const usersCollection = dbClient.database.collection('users');
+
+    const userExists = await usersCollection.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+    const newUser = {
+      email,
+      password: hashedPassword,
+    };
+
+    const result = await usersCollection.insertOne(newUser);
+    const user = result.ops[0];
+
+    return res.status(201).json({ id: user._id, email: user.email });
+  }
+}
+
+module.exports = UsersController;
